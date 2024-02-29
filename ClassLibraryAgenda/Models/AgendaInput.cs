58616c263d979d5a8408e9eb3733341f24dc61ae
richardsgab <MaraG@PC215-02>
2024-02-29 11:06:44 +0100

    Add project files.

---
+++ ClassLibraryAgenda/Models/AgendaInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryAgenda.Models
+{
+    public class AgendaInput
+    {
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public AgendaInput(string text, DateTime date)
+        {
+            Text = text;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date} - {Text}";
+        }
+    }
+}
---
+++ ClassLibraryAgenda/Models/Databank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryAgenda.Models
+{
+    public class Databank
+    {
+        private List<AgendaInput>inputs = new List<AgendaInput>();
+
+        public void AddInput(AgendaInput aginput)
+        {            
+            inputs.Add(aginput);
+        }
+        public List<AgendaInput> FindInputs(DateTime dt)
+        {
+            List<AgendaInput> results = new List<AgendaInput>();
+            for(int i = 0; i < inputs.Count; i++)  
+            {
+                var temp = dt.ToShortDateString();
+                if (inputs[i].Date.ToString() == temp.ToString())
+                {
+                    results.Add(inputs[i]);
+                }
+            }
+            return results;
+        }
+        public List<AgendaInput> DeleteInputs()
+        {
+            List<AgendaInput> results = new List<AgendaInput>();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                results.Remove(inputs[i]);
+            }
+            return results;
+        }
+
+
+
+    }
+    }
+}
